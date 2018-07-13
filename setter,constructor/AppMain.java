9b82c8bc7ffea8fd1823c23e93a064f758e5a6be
kavyaerla <39970023+kavyaerla@users.noreply.github.com>
2018-07-12 21:23:36 -0500

    Add files via upload

---
+++ setter,constructor/AppMain.java
@@ -0,0 +1,13 @@
+
+package com.jcg.spring.setter.injection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class AppMain {
+
+    
+    public static void main(String[] args) {
+    	ApplicationContext contextObj = new ClassPathXmlApplicationContext("spring-beans.xml");
+        Employee empObj = (Employee) contextObj.getBean("employeeBean");
+        empObj.showResult();
+    }
+}
